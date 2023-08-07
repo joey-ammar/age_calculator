46d5b493b742496ba1deee4f2a025190ce0af7c1
joey-ammar <xyx8703@gmail.com>
2023-08-07 10:21:16 +0200

    full project modified

--- app.js
+++ app.js
@@ -1,76 +1,159 @@
-/**Variables */
-/**Variables */
-let day = document.getElementById("day");
-let month = document.getElementById("month");
-let year = document.getElementById("year");
-let dayResult = document.getElementById("dayResult");
-let monthResult = document.getElementById("monthResult");
-let yearResult = document.getElementById("yearResult");
-let form = document.querySelector(".sumbit_btn");
-let reset = document.querySelector(".reset_container");
-/* Creating Date f */
-/* Creating Date f */
-let date = new Date();
-let getDate = date.getDate();
-let getMonth = date.getMonth();
-let getYear = date.getFullYear();
-/**getting all the month */
-/**getting all the month */
-let allMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-/**Click on the submit */
+window.onload = function () {
+  /**
+   * Variables
+   */
+  let day = document.getElementById("day");
+  let month = document.getElementById("month");
+  let year = document.getElementById("year");
+  /* lebels / span because they have all the tag name */
+  const labels = document.getElementsByTagName("label");
+  const spans = document.getElementsByTagName("span");
+  /** class name cause they all share the same class */
+  const error = document.getElementsByClassName("error");
+  const submitButton = document.getElementById("submit");
+  /**
+   * End Variables
+   */
 
-function validate() {
-  const inputs = document.querySelectorAll("input");
-  let validator = true;
-  inputs.forEach((i) => {
-    const parent = i.parentElement;
-    if (!i.value) {
-      i.style.borderColor = "red";
-      parent.querySelector("small").innerText = "this field is required.";
-      validator = false;
-    } else if (getMonth.value > 12) {
-      getMonth.style.borderColor = "red";
-      getMonth.parentElement.querySelector("small").innerText =
-        "must be valid month.";
-      validator = false;
-    } else if (getDate.value > 31) {
-      getDate.style.borderColor = "red";
-      getDate.parentElement.querySelector("small").innerText =
-        "must be valid day.";
-      validator = false;
+  /**Date object */
+  const date = new Date();
+  let currentDay = date.getDate();
+  let currentMonth = date.getMonth() + 1;
+  let currentYear = date.getFullYear();
+  /**End Date object function */
+
+  /**Dealing with errors */
+  /** ERRORS */
+  const typeOfError = [
+    "",
+    "This field is required",
+    "Must be a valid day",
+    "Must be a valid month",
+    "Must be a valid year",
+    "Must be a valid date",
+  ];
+  /* ERROR STATE */
+  const errorState = (numberOfError, typeOfDate, typeOfError, color) => {
+    error[numberOfError].innerHTML = typeOfError;
+    labels[numberOfError].style.color = color;
+    typeOfDate.style.borderColor = color;
+  };
+
+  /* IS LEAP YEAR */
+  const isLeapYear = (day, month, year) => {
+    month = month - 1;
+    fullDate = new Date(year, month, day);
+    if (
+      day == fullDate.getDate() &&
+      month == fullDate.getMonth() &&
+      year == fullDate.getFullYear()
+    )
+      return true;
+    else return false;
+  };
+
+  /**CALCULATE THE AGE FUNCTION */
+
+  const substractAge = () => {
+    let newYear = Math.abs(currentYear - year.value);
+
+    let newMonth = 0;
+    if (currentMonth >= month.value) {
+      newMonth = currentMonth - month.value;
     } else {
-      i.style.borderColor = "black";
-      parent.querySelector("small").innerText = "";
-      validator = true;
+      newYear--;
+      newMonth = 12 + currentMonth - month.value;
     }
-  });
-  return validator;
-}
 
-form.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (validate()) {
-    if (day.value > getDate) {
-      getDate = getDate + getMonth[getMonth - 1];
-      getMonth = getMonth - 1;
+    let newDay = 0;
+    if (currentDay >= day.value) {
+      newDay = currentDay - day.value;
+    } else {
+      newMonth--;
+      if (isLeapYear(day.value, month.value, year.value)) {
+        newDay = 30 + currentDay - day.value;
+      } else {
+        newDay = currentDay - day.value;
+      }
+
+      if (newMonth < 0) {
+        newMonth = 11;
+        newYear--;
+      }
+      if (newMonth < currentMonth) {
+        newDay++;
+      }
     }
-    if (month.value > getMonth) {
-      getMonth = getMonth + 12;
-      getYear = getYear - 1;
+
+    spans[0].innerHTML = newYear;
+    spans[1].innerHTML = newMonth;
+    spans[2].innerHTML = newDay;
+  };
+
+  const isDayCorrect = () => {
+    if (day.value == "") {
+      errorState(0, day, typeOfError[1], "#ff5757");
+      return false;
+    } else if (day.value <= 0 || day.value > 31) {
+      errorState(0, day, typeOfError[2], "#ff5757");
+      return false;
+    } else if (isLeapYear(day.value, month.value, year.value) == false) {
+      errorState(0, day, typeOfError[5], "#ff5757");
+      return false;
+    } else {
+      errorState(0, day, typeOfError[0], "");
+      return true;
     }
-    const d = getDate - day.value;
-    const m = getMonth - month.value;
-    const y = getYear - year.value;
-    dayResult.innerHTML = d;
-    monthResult.innerHTML = m;
-    yearResult.innerHTML = y;
-  }
-});
-/**click on the reset */
-/**validation */
-/**Reset btn */
-reset.addEventListener("click", () => {
-  window.addEventListener("load", () => {
-    console.log("page is fully loaded");
+  };
+
+  const isMonthCorrect = () => {
+    if (month.value == "") {
+      errorState(1, month, typeOfError[1], "#ff5757");
+      return false;
+    } else if (month.value <= 0 || month.value > 12) {
+      errorState(1, month, typeOfError[3], "#ff5757");
+      return false;
+    } else if (isLeapYear(day.value, month.value, year.value) == false) {
+      errorState(1, month, typeOfError[0], "#ff5757");
+      return false;
+    } else {
+      errorState(1, month, typeOfError[0], "");
+      return true;
+    }
+  };
+
+  const isYearCorrect = () => {
+    if (year.value == "") {
+      errorState(2, year, typeOfError[1], "#ff5757");
+      return false;
+    } else if (year.value > currentYear) {
+      errorState(2, year, typeOfError[4], "#ff5757");
+      return false;
+    } else if (year.value == currentYear && month.value > currentMonth) {
+      errorState(1, month, typeOfError[3], "#ff5757");
+      return false;
+    } else if (
+      year.value == currentYear &&
+      month.value == currentMonth &&
+      day.value > currentDay
+    ) {
+      errorState(0, day, typeOfError[2], "#ff5757");
+      return false;
+    } else if (isLeapYear(day.value, month.value, year.value) == false) {
+      errorState(2, year, typeOfError[0], "#ff5757");
+      return false;
+    } else {
+      errorState(2, year, typeOfError[0], "");
+      return true;
+    }
+  };
+
+  submitButton.addEventListener("click", () => {
+    isDayCorrect();
+    isMonthCorrect();
+    isYearCorrect();
+    if (isDayCorrect() && isMonthCorrect() && isYearCorrect()) {
+      substractAge();
+    }
   });
-});
+};
